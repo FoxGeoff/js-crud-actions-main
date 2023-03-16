a8dae5428a6cd5e5c887fe0f71548ac4a2e6b5d0
Geoff Fox <fox.geoffrey@gmail.com>
2023-03-16 13:31:54 -0700

    products.js refactored

--- readme.md
+++ readme.md
@@ -109,4 +109,35 @@ module.exports = function (db) {
 
 ```Javascript
 /* re-factored class */
+var express = require('express');
+var router = express.Router();
+
+module.exports = function (db) {
+  router
+    .route('/products')
+    .get((req, res) => {
+      res.send(db.get('products').value());
+    })
+    .post((req, res) => {
+      const newProduct = req.body;
+      res.send(db.get('products').insert(newProduct).write());
+    });
+
+  router
+  .route('/products/:id')
+  .patch((req, res) => {
+    res.send(
+      db.get('products').find({ id: req.params.id }).assign(req.body).write()
+    );
+  })
+  .delete((req, res) => {
+    db.get('products').remove({ id: req.params.id }).write();
+    res.status().send();
+  }).get((req, res) => {
+    res.send(db.get('products').find({ id: req.params.id }).value());
+  });
+
+  return router;
+};
+
 ```
--- routes/products.js
+++ routes/products.js
@@ -3,27 +3,26 @@ var router = express.Router();
 
 module.exports = function (db) {
   router
-  .route('/products')
-  .get((req, res) => {
-    res.send(db.get('products').value());
-  })
-  .post((req, res) => {
-    const newProduct = req.body;
-    res.send(db.get('products').insert(newProduct).write());
-  });
+    .route('/products')
+    .get((req, res) => {
+      res.send(db.get('products').value());
+    })
+    .post((req, res) => {
+      const newProduct = req.body;
+      res.send(db.get('products').insert(newProduct).write());
+    });
 
-  router.patch('/products/:id', (req, res) => {
+  router
+  .route('/products/:id')
+  .patch((req, res) => {
     res.send(
       db.get('products').find({ id: req.params.id }).assign(req.body).write()
     );
-  });
-
-  router.delete('/products/:id', (req, res) => {
+  })
+  .delete((req, res) => {
     db.get('products').remove({ id: req.params.id }).write();
     res.status().send();
-  });
-
-  router.get('/products/:id', (req, res) => {
+  }).get((req, res) => {
     res.send(db.get('products').find({ id: req.params.id }).value());
   });
 
