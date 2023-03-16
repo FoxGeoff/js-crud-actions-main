c947d8c4451fcfe6d13d2b413ced078342a88b9d
Geoff Fox <fox.geoffrey@gmail.com>
2023-03-16 13:27:16 -0700

    products.js refactored

--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function (db) {
-  router.get('/products', (req, res) => {
+  router
+  .route('/products')
+  .get((req, res) => {
     res.send(db.get('products').value());
-  });
-
-  router.post('/products', (req, res) => {
+  })
+  .post((req, res) => {
     const newProduct = req.body;
     res.send(db.get('products').insert(newProduct).write());
   });
